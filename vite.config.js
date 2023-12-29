b9718efe87883edc1923b3202b8013afe5ffa0e7
Eleazarguitar18 <eleazarguitar18@gmail.com>
2023-12-29 17:08:49 -0400

    config for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/https://Eleazarguitar18.github.io/React-Task/'
 })
