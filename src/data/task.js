4252bf5d66fa20170fcc607fb4c249c2b25c929c
Eleazarguitar18 <eleazarguitar18@gmail.com>
2024-02-24 23:53:09 -0400

    url a produccion

--- src/data/task.js
+++ src/data/task.js
@@ -17,29 +17,28 @@ import axios from "axios";
 //     },
 // ]
 
-
-// export let urlbase = 'https://taskbackend-5jt6.onrender.com'
-export const urlbase = 'http://localhost:3000';
+export let urlbase = "https://taskbackend-5jt6.onrender.com";
+// export const urlbase = 'http://localhost:3000';
 
 const getTaskData = async () => {
-    try {
-        const response = await axios.get(`${urlbase}/api/task`);
+  try {
+    const response = await axios.get(`${urlbase}/api/task`);
 
-        // Axios ya maneja automáticamente los códigos de estado y lanza un error en caso de un código no exitoso
-        // Por lo tanto, no necesitas verificar manualmente response.ok
+    // Axios ya maneja automáticamente los códigos de estado y lanza un error en caso de un código no exitoso
+    // Por lo tanto, no necesitas verificar manualmente response.ok
 
-        // Obtén directamente los datos de la respuesta
-        const data = response.data;
+    // Obtén directamente los datos de la respuesta
+    const data = response.data;
 
-        // Puedes realizar más acciones con los datos aquí
-        // console.log(data);
+    // Puedes realizar más acciones con los datos aquí
+    // console.log(data);
 
-        // Exportar los datos
-        return data;
-    } catch (error) {
-        console.error('Error al obtener los datos:', error);
-        throw error; // Puedes lanzar el error nuevamente si es necesario
-    }
+    // Exportar los datos
+    return data;
+  } catch (error) {
+    console.error("Error al obtener los datos:", error);
+    throw error; // Puedes lanzar el error nuevamente si es necesario
+  }
 };
 // const fetchData = async () => {
 //     try {
